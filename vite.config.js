5495bb7d25e741bfceae4986a4f9e9dd4a8f2d2a
ielizari <ielizari28@gmail.com>
2024-03-19 22:34:16 +0100

    Added vite-plugin-singlefile. Fixed vite build

--- vite.config.js
+++ vite.config.js
@@ -4,30 +4,19 @@ import path from 'node:path'
 import process from 'node:process'
 import { viteSingleFile } from "vite-plugin-singlefile"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     viteSingleFile(),
   ],
-  root: 'src/view',
-  publicDir: 'src/view/public',
-  build: {
-    outDir: 'src/view/dist',
-    rollupOptions: {
-      input: {
-        app: 'src/index.html',
-      }
-    }
-  },
+  root: 'src/view/vue',
   resolve: {
     alias: {
-      '/src': path.resolve(process.cwd(), 'src'),
       '@report': path.resolve(process.cwd(), 'report'),
-      '@stores': path.resolve(process.cwd(), 'src/view/src/stores'),
-      '@components': path.resolve(process.cwd(), 'src/view/src/components'),
-      '@composables': path.resolve(process.cwd(), 'src/view/src/composables'),
-      '@utils': path.resolve(process.cwd(), 'src/view/src/utils'),
+      '@stores': path.resolve(process.cwd(), 'src/view/vue/src/stores'),
+      '@components': path.resolve(process.cwd(), 'src/view/vue/src/components'),
+      '@composables': path.resolve(process.cwd(), 'src/view/vue/src/composables'),
+      '@utils': path.resolve(process.cwd(), 'src/view/vue/src/utils'),
     }
   },
   css: {
